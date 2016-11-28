5b575109a3db79c08a554fa417f4fc5f650b7e7c
Takashi Toyoshima <toyoshim@gmail.com>
2016-11-28 20:02:27 +0000

    mobile support

--- src/iocs.js
+++ src/iocs.js
@@ -1,54 +1,179 @@
 /* global magic2 */
 /* global navigator */
 (function() {
+  var touchbits = 0xff;
+  var touches = {
+    esc: {
+      id: -1,
+      pressed: false
+    },
+    f5: {
+      id: -1,
+      pressed: false
+    },
+    z: { id: -1 },
+    x: { id: -1 },
+    axes: {
+      x: 0,
+      y: 0,
+      id: -1
+    }
+  };
+
+  document.addEventListener('touchstart', function(e) {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    for (var touch of e.changedTouches) {
+      if (touch.clientY < (h / 3)) {
+        if (touch.clientX < (w / 2)) {
+          touches.esc.id = touch.identifier;
+          console.log('esc on:', touches.esc.id);
+          touches.esc.pressed = true;
+        } else {
+          touches.f5.id = touch.identifier;
+          touches.f5.pressed = true;
+        }
+        return;
+      }
+      if (touch.clientX < (w / 2)) {
+        touches.axes.id = touch.identifier;
+        touches.axes.x = touch.clientX;
+        touches.axes.y = touch.clientY;
+      } else if (touch.clientX > (w * 3 / 4)) {
+        touches.z.id = touch.identifier;
+        touchbits &= ~0x40;
+      } else {
+        touches.x.id = touch.identifier;
+        touchbits &= ~0x20;
+      }
+    }
+  }, false);
+
+  document.addEventListener('touchend', function(e) {
+    for (var touch of e.changedTouches) {
+      var id = touch.identifier;
+      console.log('end:', id);
+      if (id == touches.esc.id) {
+        console.log('esc off');
+        touches.esc.id = -1;
+        touches.esc.pressed = false;
+      } else if (id == touches.f5.id) {
+        touches.f5.id = -1;
+        touches.f5.pressed = false;
+      } else if (id == touches.axes.id) {
+        touches.axes.id = -1;
+        touchbits |= 0x0f;
+      } else if (id == touches.z.id) {
+        touchbits |= 0x40;
+        touches.z.id = -1;
+      } else if (id == touches.x.id) {
+        touchbits |= 0x20;
+        touches.x.id = -1;
+      }
+    }
+  }, false);
+
+  document.addEventListener('touchmove', function(e) {
+    var range = 32;
+    e.preventDefault();
+    for (var touch of e.changedTouches) {
+      if (touch.identifier != touches.axes.id)
+        continue;
+      if (touch.clientX > (touches.axes.x + range)) {
+        touchbits &= ~0x08;
+        touchbits |= 0x04;
+      } else if (touch.clientX < (touches.axes.x - range)) {
+        touchbits |= 0x08;
+        touchbits &= ~0x04;
+      } else {
+        touchbits |= 0x0c;
+      }
+      if (touch.clientY > (touches.axes.y + range)) {
+        touchbits &= ~0x02;
+        touchbits |= 0x01;
+      } else if (touch.clientY < (touches.axes.y - range)) {
+        touchbits |= 0x02;
+        touchbits &= ~0x01;
+      } else {
+        touchbits |= 0x03;
+      }
+    }
+  }, false);
+
   var keyStates = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  var EdgeKeyToCode = function(key) {
+    switch (key) {
+      case 'Down':
+      case 'Left':
+      case 'Right':
+      case 'Up':
+        return 'Arrow' + key;
+      case 'Esc':
+        return 'Escape';
+      case 'x':
+        return 'KeyX';
+      case 'z':
+        return 'KeyZ';
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+        return 'Digit' + key;
+      default:
+        return key;
+    }
+  }
 
   // ESC, F1-F5
   window.addEventListener('keydown', function(e) {
+    if (!e.code)
+      e.code = EdgeKeyToCode(e.key);
     switch (e.code) {
-      case "ArrowDown":
+      case 'ArrowDown':
         keyStates[7] |= (1 << 6);
         keyStates[9] |= (1 << 4);  // Num 2
         break;
-      case "ArrowLeft":
+      case 'ArrowLeft':
         keyStates[7] |= (1 << 3);
         keyStates[8] |= (1 << 7);  // Num 4
         break;
-      case "ArrowRight":
+      case 'ArrowRight':
         keyStates[7] |= (1 << 5);
         keyStates[9] |= (1 << 1);  // Num 6
         break;
-      case "ArrowUp":
+      case 'ArrowUp':
         keyStates[7] |= (1 << 4);
         keyStates[8] |= (1 << 4);  // Num 8
         break;
-      case "Escape":
+      case 'Escape':
         keyStates[0] |= (1 << 1);
         break;
-      case "F1":
-      case "Digit1":
+      case 'F1':
+      case 'Digit1':
         keyStates[12] |= (1 << 3);
         break;
-      case "F2":
-      case "Digit2":
+      case 'F2':
+      case 'Digit2':
         keyStates[12] |= (1 << 4);
         break;
-      case "F3":
-      case "Digit3":
+      case 'F3':
+      case 'Digit3':
         keyStates[12] |= (1 << 5);
         break;
-      case "F4":
-      case "Digit4":
+      case 'F4':
+      case 'Digit4':
         keyStates[12] |= (1 << 6);
         break;
-      case "F5":
-      case "Digit5":
+      case 'F5':
+      case 'Digit5':
         keyStates[12] |= (1 << 7);
         break;
-      case "KeyX":
+      case 'KeyX':
         keyStates[5] |= (1 << 3);
         break;
-      case "KeyZ":
+      case 'KeyZ':
         keyStates[5] |= (1 << 2);
         break;
       default:
@@ -58,50 +183,52 @@
   }, false);
 
   window.addEventListener('keyup', function(e) {
+    if (!e.code)
+      e.code = EdgeKeyToCode(e.key);
     switch (e.code) {
-      case "ArrowDown":
+      case 'ArrowDown':
         keyStates[7] &= ~(1 << 6);
         keyStates[9] &= ~(1 << 4);  // Num 2
         break;
-      case "ArrowLeft":
+      case 'ArrowLeft':
         keyStates[7] &= ~(1 << 3);
         keyStates[8] &= ~(1 << 7);  // Num 4
         break;
-      case "ArrowRight":
+      case 'ArrowRight':
         keyStates[7] &= ~(1 << 5);
         keyStates[9] &= ~(1 << 1);  // Num 6
         break;
-      case "ArrowUp":
+      case 'ArrowUp':
         keyStates[7] &= ~(1 << 4);
         keyStates[8] &= ~(1 << 4);  // Num 8
         break;
-      case "Escape":
+      case 'Escape':
         keyStates[0] &= ~(1 << 1);
         break;
-      case "F1":
-      case "Digit1":
+      case 'F1':
+      case 'Digit1':
         keyStates[12] &= ~(1 << 3);
         break;
-      case "F2":
-      case "Digit2":
+      case 'F2':
+      case 'Digit2':
         keyStates[12] &= ~(1 << 4);
         break;
-      case "F3":
-      case "Digit3":
+      case 'F3':
+      case 'Digit3':
         keyStates[12] &= ~(1 << 5);
         break;
-      case "F4":
-      case "Digit4":
+      case 'F4':
+      case 'Digit4':
         keyStates[12] &= ~(1 << 6);
         break;
-      case "F5":
-      case "Digit5":
+      case 'F5':
+      case 'Digit5':
         keyStates[12] &= ~(1 << 7);
         break;
-      case "KeyX":
+      case 'KeyX':
         keyStates[5] &= ~(1 << 3);
         break;
-      case "KeyZ":
+      case 'KeyZ':
         keyStates[5] &= ~(1 << 2);
         break;
       default:
@@ -111,7 +238,12 @@
   }, false);
 
   window.iocs_bitsns = function(group) {
-    return keyStates[group];
+    var touch = 0;
+    if (group == 0 && touches.esc.pressed)
+      touch |= (1 << 1);
+    if (group == 12 && touches.f5.pressed)
+      touch |= (1 << 7);
+    return keyStates[group] | touch;
   };
 
   var contrast = {
@@ -138,10 +270,10 @@
 
   window.iocs_joyget = function (id) {
     if (!navigator.getGamepads)
-      return 0xff;
+      return touchbits;
     var pad = navigator.getGamepads()[id];
     if (!pad)
-      return 0xff;
+      return touchbits;
     var bits = 0xff;
     var x = pad.axes[0] || 0;
     var y = pad.axes[1] || 0;
@@ -159,7 +291,7 @@
       bits &= ~0x40;
     if (b)
       bits &= ~0x20;
-    return bits;
+    return bits & touchbits;
   };
 
   var sprites = new Array(128);
